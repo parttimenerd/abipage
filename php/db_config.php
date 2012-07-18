2c2caa3119b3418360fce146b392920e1c713332
parttimenerd <cato-der-kater@online.de>
2012-07-18 02:17:19 +0200

    D:/Code/t.txt

--- php/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("DB_HOST", "localhost");
-define("DB_NAME", "db");
-define("DB_USER", "Root");
-define("DB_PASSWORD", "");
-define("DB_PREFIX", "abipageasdiutgfsadfasdf_");
-define("URL", "http://localhost/abipage");
